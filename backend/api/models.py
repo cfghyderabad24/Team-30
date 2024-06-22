81d3ccacb7bda4f3fb9c1bf13c59419fa1bf2910
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-06-22 17:52:49 +0530

    Added model

---
+++ backend/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Seed(models.Model):
+    variety = models.CharField(max_length=100)
+    quantity = models.FloatField()
+
+    class Meta:
+        abstract = True
+
+class Farmer(models.Model):
+    name = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+    aadhar_no = models.CharField(max_length=12)
+    contact_no = models.CharField(max_length=10)
+    area_ploughed = models.FloatField()
+    season = models.CharField(max_length=100)
+    crop_grown = models.CharField(max_length=100)
+    seeds_used = models.ArrayField(
+        model_container=Seed,
+    )
+    date_of_seed_sown = models.DateField()
+    transplanting_method = models.CharField(max_length=50, choices=[('manual', 'Manual'), ('machine', 'Machine')])
+    irrigation_method = models.CharField(max_length=50, choices=[('borewell', 'Borewell'), ('drip', 'Drip'), ('well', 'Well'), ('river', 'River'), ('rain', 'Rain')])
+    fertilizers_used = models.CharField(max_length=50, choices=[('organic', 'Organic'), ('chemical', 'Chemical'), ('bioinputs', 'Bioinputs')])
+    date_of_harvesting = models.DateField()
+    yield_kg = models.FloatField()
