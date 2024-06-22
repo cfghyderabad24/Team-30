80bafe6825ccd601a5b98a7452bb46d8dfbc82b7
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-06-22 20:08:42 +0530

    added http put and delete methods

--- backend/api/views.py
+++ backend/api/views.py
@@ -22,3 +22,21 @@ def users_list(request):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['PUT', 'DELETE'])
+def users_detail(request, id):
+    try:
+        user = Farmer.objects.get(id=id)
+    except Farmer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = FarmerSerializer(user, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
