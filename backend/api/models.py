09addde896441e72e4e311e4b920c621ccf2cf29
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-06-23 00:22:07 +0530

    updated views.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Farmer(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     pincode = models.CharField(max_length=6)
     aadhar_no = models.CharField(max_length=12)
