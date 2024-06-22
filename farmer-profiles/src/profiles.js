f9e085336073b2386b0f263f86cde790e1a63ee5
Shubham Kumar Sahoo <shubhamkrsahoo100@gmail.com>
2024-06-23 03:14:25 +0530

    Add files via upload

---
+++ farmer-profiles/src/profiles.js
@@ -0,0 +1,53 @@
+// src/profiles.js
+const profiles = [
+    {
+        id: 1,
+        name: 'Ajay Raj',
+        age: 26,
+        gender: 'Male',
+        location: 'Punjab',
+        image: 'img1.jpg'
+    },
+    {
+        id: 2,
+        name: 'Jaya Rani',
+        age: 28,
+        gender: 'Female',
+        location: 'Chhattisgarh',
+        image: 'img2.jpg'
+    },
+    {
+        id: 3,
+        name: 'Ronny Singh',
+        age: 26,
+        gender: 'Male',
+        location: 'Pune',
+        image: 'img3.jpg'
+    },
+    {
+        id: 4,
+        name: 'Emily White',
+        age: 26,
+        gender: 'Female',
+        location: 'Kansas',
+        image: 'img4.jpg'
+    },
+    {
+        id: 5,
+        name: 'Aditya',
+        age: 26,
+        gender: 'Male',
+        location: 'Noida',
+        image: 'img5.jpg'
+    },
+    {
+        id: 6,
+        name: 'David',
+        age: 26,
+        gender: 'Male',
+        location: 'Khorda',
+        image: 'img6.jpg'
+    }
+];
+
+export default profiles;
