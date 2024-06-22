256a449c10abd43dad352efe39a17fdcae3858b6
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-06-22 21:45:29 +0530

    added another method for dashboard in ngo

--- backend/ngo/urls.py
+++ backend/ngo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('ngo/', views.users_list),
+    path('ngo/<str:username>&<str:password>', views.users_list),
+    path('ngo/',views.list),
 ]
--- backend/ngo/views.py
+++ backend/ngo/views.py
@@ -1,11 +1,21 @@
-from rest_framework import viewsets, status
+from rest_framework import  status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from api.models import Farmer
+from api.serializers import FarmerSerializer
 
 
-@api_view(['GET'])
+
 def users_list(request, username, password):
     if username == "admin" & password == "admin":
         return Response({'success': 'Login Success'}, status=status.HTTP_200_OK)
     else:
         return Response({'error': 'Login failed'}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET', 'POST'])
+def list(request):
+    if request.method == 'GET':
+        data = Farmer.objects.all()
+        serializer = FarmerSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+
